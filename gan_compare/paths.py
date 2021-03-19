ce5574972e5495ff0b0f7509bd4092adc20a6174
Richard Osuala <Richard.Osuala@de.ibm.com>
2021-03-19 11:56:38 +0100

    no-assume-unchanged for tracking gan_compare/paths.py

--- gan_compare/paths.py
+++ gan_compare/paths.py
@@ -1,11 +1,3 @@
-#INBREAST_IMAGE_PATH = "path/to/InBreast/InBreast/AllDICOMs/"
-#INBREAST_XML_PATH = "path/to/InBreast/InBreast/AllXML/"
-#INBREAST_ROI_PATH = "path/to/Desktop/gan_aug/InBreast/InBreast/AllROI/"
-
-#INBREAST_IMAGE_PATH = "/Users/richardosuala/Desktop/Datasets/InBreast/AllDICOMs/"
-#INBREAST_XML_PATH = "/Users/richardosuala/Desktop/Datasets/InBreast/AllXML/"
-#INBREAST_ROI_PATH = "/Users/richardosuala/Desktop/Datasets/InBreast/AllROI/"
-
-INBREAST_IMAGE_PATH = "/home/roo/data/InBreast/AllDICOMs/"
-INBREAST_XML_PATH = "/home/roo/data/InBreast/AllXML/"
-INBREAST_ROI_PATH = "/home/roo/data/InBreast/AllROI/"
+INBREAST_IMAGE_PATH = "path/to/InBreast/InBreast/AllDICOMs/"
+INBREAST_XML_PATH = "path/to/InBreast/InBreast/AllXML/"
+INBREAST_ROI_PATH = "path/to/Desktop/gan_aug/InBreast/InBreast/AllROI/"
