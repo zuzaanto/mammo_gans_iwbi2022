Enable binary classification of healthy (1) vs. non-healthy (0). Fix and move label calculation to base_dataset. Add exemplary config yaml.

gan_compare/dataset/bcdr_dataset.py
@@ -23,6 +23,7 @@ class BCDRDataset(BaseDataset):
         margin: int = 100,
         final_shape: Tuple[int, int] = (400, 400),
         conditional_birads: bool = False,
+        classify_binary_healthy: bool = False,
         split_birads_fours: bool = False,  # Setting this to True will result in BiRADS annotation with 4a, 4b, 4c split to separate classes
         is_trained_on_calcifications: bool = False,
         is_trained_on_masses: bool = True,
@@ -36,6 +37,7 @@ class BCDRDataset(BaseDataset):
             min_size=min_size,
             margin=margin,
             final_shape=final_shape,
+            classify_binary_healthy=classify_binary_healthy,
             conditional_birads=conditional_birads,
             split_birads_fours=split_birads_fours,
             is_trained_on_calcifications=is_trained_on_calcifications,
@@ -95,7 +97,6 @@ class BCDRDataset(BaseDataset):
         mask = cv2.resize(mask, self.final_shape, interpolation=cv2.INTER_AREA)
         sample = torchvision.transforms.functional.to_tensor(image[..., np.newaxis])
 
-        if self.transform:
-            sample = self.transform(sample)
+        if self.transform: sample = self.transform(sample)
 
         return sample, image

gan_compare/dataset/inbreast_dataset.py
@@ -21,9 +21,10 @@ class InbreastDataset(BaseDataset):
         min_size: int = 160,
         margin: int = 100,
         final_shape: Tuple[int, int] = (400, 400),
+        classify_binary_healthy: bool = False,
         conditional_birads: bool = False,
         split_birads_fours: bool = False,  # Setting this to True will result in BiRADS annotation with 4a, 4b, 4c split to separate classes
-        is_trained_on_calcifications: bool = False,
+        is_trained_on_calcifications: bool = True,
         is_trained_on_masses: bool = True,
         is_trained_on_other_roi_types: bool = False,
         is_condition_binary:bool = False,
@@ -35,6 +36,7 @@ class InbreastDataset(BaseDataset):
             min_size=min_size,
             margin=margin,
             final_shape=final_shape,
+            classify_binary_healthy=classify_binary_healthy,
             conditional_birads=conditional_birads,
             split_birads_fours=split_birads_fours,
             is_trained_on_calcifications=is_trained_on_calcifications,
@@ -92,20 +94,8 @@ class InbreastDataset(BaseDataset):
 
         sample = torchvision.transforms.functional.to_tensor(image[..., np.newaxis])
 
-        if self.transform:
-            sample = self.transform(sample)
-        if self.conditional_birads:
-            if self.is_condition_binary:
-                condition = metapoint["birads"][0]
-                if int(condition) <= 3:
-                    return sample, 0
-                return sample, 1
-            elif self.split_birads_fours:
-                condition = BIRADS_DICT[metapoint["birads"]]
-            else:
-                condition = metapoint["birads"][
-                    0
-                ]  # avoid 4c, 4b, 4a and just truncate them to 4
-            return sample, int(condition), image
+        if self.transform: sample = self.transform(sample)
 
-        return sample, image
+        label = self.determine_label(metapoint)
+
+        return sample, label

gan_compare/scripts/metrics.py
@@ -6,10 +6,10 @@ import torch
 def calc_all_scores(y_true, y_prob_logit, v_loss, run_type, epoch=None):
     y_prob = torch.exp(y_prob_logit) # probabilities of all classes for each sample
     _, y_pred = torch.max(y_prob, 1) # class prediction for each sample
-    v_loss = calc_loss(v_loss, run_type, epoch)
+    loss = calc_loss(v_loss, run_type, epoch)
     acc_score = calc_accuracy(y_true, y_pred, run_type, epoch)
     prec_rec_f1_scores = calc_prec_rec_f1_scores(y_true, y_pred, run_type, epoch)
-    return v_loss, acc_score, prec_rec_f1_scores
+    return loss, acc_score, prec_rec_f1_scores
 
 def calc_loss(v_loss, run_type, epoch=None):
     loss = np.mean(v_loss)