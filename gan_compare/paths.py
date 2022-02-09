cf1b04d289ce16b0980af788c301a20d3126930e
Benji4 <bennetbreier@online.de>
2022-02-09 18:26:27 +0100

    add Bennet's paths as comment

--- gan_compare/paths.py
+++ gan_compare/paths.py
@@ -12,6 +12,9 @@ BCDR_ROOT_PATH = Path("/mnt/ml-team/homes/zuzanna.szafranowska/mammo_gans/datase
 # INBREAST_ROOT_PATH = Path("/Users/richardosuala/Desktop/Datasets/InBreast/")
 # BCDR_ROOT_PATH = Path("/Users/richardosuala/Desktop/Datasets/BCDR/")
 
+# INBREAST_ROOT_PATH = Path("C:/Users/Ben_Ji/Documents/Mammogan storage/InBreast/")
+# BCDR_ROOT_PATH = Path("C:/Users/Ben_Ji/Documents/Mammogan storage/BCDR/BCDR/")
+
 INBREAST_IMAGE_PATH = INBREAST_ROOT_PATH / "AllDICOMs/"
 INBREAST_XML_PATH = INBREAST_ROOT_PATH / "AllXML/"
 INBREAST_ROI_PATH = INBREAST_ROOT_PATH / "AllROI/"
